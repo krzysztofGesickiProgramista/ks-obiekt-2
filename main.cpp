1b136dc9dd41d80f908b68d3f02f7405fb30d408
Krzysztof Gesicki <gesicki.krzysztof@gmail.com>
2020-05-02 12:41:25 +0200

    Added feature load contacts from file

--- main.cpp
+++ main.cpp
@@ -8,14 +8,41 @@ int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
 
+    ///////////////////////////////////////////////////////////////////
+    // SPRAWDZENIE CZY ZADANE FUNKCJONALNOSCI DZIALAJA
+
+    // REJESTRACJA NOWEGO UZYTKOWNIKA:
+    cout << endl << "----- REJESTRACJA NOWEGO UZYTKOWNIKA -----" << endl;
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+
+    // WYPISANIE WSZYSTKICH UZYTKOWNIKOW:
+    cout << endl << "----- WYPISZ WSZYSTKICH UZYTKOWNIKOW -----" << endl;
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
 
+    // ZALOGOWANIE UZYTKOWNIKA:
+    cout << endl << "----- LOGOWANIE -----" << endl;
     ksiazkaAdresowa.logowanieUzytkownika();
 
-    ksiazkaAdresowa.dodajAdresata();
+    // DODWANIE DWOCH NOWYCH ADRESATOW:
+    cout << endl << "-----DODAJ ADRESATA -----" << endl;
     ksiazkaAdresowa.dodajAdresata();
     ksiazkaAdresowa.dodajAdresata();
 
+    // WYSWIETLENIE WSZYSTKICH ADRESATOW:
+    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
+
+    // WYLOGOWANIE:
+    ksiazkaAdresowa.wylogowanieUzytkownika();
+
+    // WYPISANIE WSZYSTKICH UZYTKOWNIKOW:
+    cout << endl << "----- WYPISZ WSZYSTKICH UZYTKOWNIKOW -----" << endl;
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+
+    // ZALOGOWANIE UZYTKOWNIKA:
+    cout << endl << "----- LOGOWANIE -----" << endl;
+    ksiazkaAdresowa.logowanieUzytkownika();
+
+    // WYSWIETLENIE WSZYSTKICH ADRESATOW:
     ksiazkaAdresowa.wyswietlWszystkichAdresatow();
 
     return 0;
